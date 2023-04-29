9ac32958693cc2029cb91d10d9e0943ab24a6b1d
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2023-04-29 14:34:37 -0300

    camera cinemachine

---
+++ Assets/src/waypoints/Waypoints.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Transform[] nextPoint;
+    public int layer;
+
+    private void OnDrawGizmos()
+    {
+        foreach (Transform t in transform)
+        {
+
+        }
+    }
+}
